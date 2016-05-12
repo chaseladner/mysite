e7e21a29628a257d428790ae7180fa68f8eabd34
Chase Ladner <chaseladner@Chases-MacBook-Pro.local>
2016-05-12 16:56:21 -0500

    Added meta data

--- header.php
+++ header.php
@@ -1,13 +1,21 @@
 
 <html>
     <head>
+
+		
+		<meta charset="utf-8">
+	    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+	    <meta name="viewport" content="width=device-width, initial-scale=1">
+
         <title>Your Web Developer</title>
+	
 
         <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
 		<link href="//netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css" rel="stylesheet">
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.1/css/font-awesome.min.css">
 
         <link rel="stylesheet" href="style/base.css">
+
 
 
     </head>
